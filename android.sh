033d8c647751ef25b4a1d04608d37a8f2d697b18
jmulro <jmulro@gmail.com>
2013-07-12 13:39:16 +0800

    fix iOS & marmalade & android build.

--- android.sh
+++ android.sh
@@ -7,7 +7,7 @@ NDKABI=4
 NDKVER=$NDK/toolchains/arm-linux-androideabi-4.6
 NDKP=$NDKVER/prebuilt/darwin-x86/bin/arm-linux-androideabi-
 NDKF="--sysroot $NDK/platforms/android-$NDKABI/arch-arm"
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux clean
-make HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux
+make -j -C ./ HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux TARGET=arm TARGET_SONAME=libluajit.so clean
+make -j -C ./ HOST_CC="gcc -m32" CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux TARGET=arm TARGET_SONAME=libluajit.so BUILDMODE=dynamic
 
-#mv $LUAJIT/src/libluajit.so $BUILD_DIR/
+mv $LUAJIT/src/libluajit.so $BUILD_DIR/libluajit.so
